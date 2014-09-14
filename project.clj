7ead4ef5c7d6a2017793b2321df412992e08bff1
Rosnec <rosnec.dev@gmail.com>
2014-09-13 20:27:31 -0400

    Initialized project

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject mini-java "0.1.0-SNAPSHOT"
+  :description "A MiniJava compiler implemented in Clojure."
+  :url "https://github.com/dwysocki/mini-java"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [instaparse "1.3.4"]]
+  :main mini-java.core)
---
+++ test/mini-java/core_test.clj
@@ -0,0 +1,7 @@
+(ns mini-java.core-test
+  (:use clojure.test
+        mini-java.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
