f4fec5e1a4676be4b100b3b09c4501bf10fd51d5
Rosnec <rosnec.dev@gmail.com>
2014-09-22 18:59:48 -0400

    Stuff

--- src/mini_java/core.clj
+++ src/mini_java/core.clj
@@ -8,7 +8,7 @@
 
 (defn -main
   ([source-file & args]
-     (let [grammar (clojure.java.io/resource "resources/mini-java2.instaparse")
+     (let [grammar (clojure.java.io/resource "resources/mini-java.instaparse")
            parser  (insta/parser grammar :auto-whitespace :standard)
            ast     (parser (slurp source-file))
            ast     (insta/transform trans ast)]
