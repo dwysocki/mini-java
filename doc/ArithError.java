db810a83ec2dab5720f5e7b8a8c57da2ef2cd38c
Rosnec <rosnec.dev@gmail.com>
2014-09-23 23:23:51 -0400

    Enabled total parsing on error, and created a sample java program with an error

---
+++ doc/ArithError.java
@@ -0,0 +1,25 @@
+class ArithError
+{
+    public static void main(String[] args)
+    {
+	System.out.println(new Ar().compute());
+    }
+}
+
+class Ar
+{
+    public int compute()
+    {
+	int a;
+	int b;
+	int ;c;
+	int[] d;
+
+	a = 1;
+	b = 2;
+	c = 3;
+
+	return ((a*b)+(b*c))*(a-b);
+
+    }
+}
--- src/mini_java/core.clj
+++ src/mini_java/core.clj
@@ -10,8 +10,7 @@
   ([source-file & args]
      (let [grammar (clojure.java.io/resource "resources/mini-java.instaparse")
            parser  (insta/parser grammar :auto-whitespace :standard)
-           ast     (parser (slurp source-file))
+           ast     (parser (slurp source-file)
+                           :total true)
            ast     (insta/transform trans ast)]
-       (if-not (insta/failure? ast)
-         (insta/visualize ast)
-         ast))))
+       (insta/visualize ast))))
