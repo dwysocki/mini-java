cf14653cf82a812569766f895838080ed72b67b8
Rosnec <rosnec.dev@gmail.com>
2014-12-05 00:34:20 -0500

    Incrementing version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mini-java "0.2.3"
+(defproject mini-java "0.3.0"
   :description "A MiniJava compiler implemented in Clojure."
   :url "https://github.com/dwysocki/mini-java"
   :license {:name "MIT License"
