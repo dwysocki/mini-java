655ede2e4f002d4e85873c9a9646c2a8bf01e4c9
Rosnec <rosnec.dev@gmail.com>
2014-12-10 01:39:49 -0500

    Added switches to sample for running with mini-javac, javac, or both

--- samples/sample-run.sh
+++ samples/sample-run.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+__compilers__="mini-javac javac"
+function compile {
+    __cmd1=$1
+    __cmd2=$2
+    # display and evaluate compile/run command
+    echo -e "${__cmd1} &&\n${__cmd2}"
+    eval "${__cmd1} && ${__cmd2}"
+}
+
+function compile_switch {
+    case "$1" in
+        mini-javac)
+            compile "java -jar ../target/mini-javac.jar -d target $2" \
+                    "java -cp target $3"
+            ;;
+        javac)
+            compile "javac -d target $2" \
+                    "java -cp target $3"
+            ;;
+        all)
+            # compile all
+            compile_switch mini-javac $2 $3
+            compile_switch      javac $2 $3
+            ;;
+        *)
+            echo "Usage: $0 {mini-javac|javac|all}"
+            exit 1
+    esac
+}
+
+
 # Compiles and runs all of the sample programs
 # Displays the commands being run
 
@@ -9,10 +40,12 @@ for f in $FILES
 do
     fname=$(basename "$f")
     class=$(basename "$f" .java)
-    cmd1="java -jar ../target/mini-javac.jar -d target $f"
-    cmd2="java -cp target $class"
-    cmdstr="${cmd1} &&\n${cmd2}"
-    cmd="${cmd1} && ${cmd2}"
-    echo -e $cmdstr
-    eval $cmd
+    compile_switch $1 $f $class
+            
+#    cmd1="java -jar ../target/mini-javac.jar -d target $f"
+#    cmd2="java -cp target $class"
+#    cmdstr="${cmd1} &&\n${cmd2}"
+#    cmd="${cmd1} && ${cmd2}"
+#    echo -e $cmdstr
+#    eval $cmd
 done
